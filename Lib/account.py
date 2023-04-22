f1fbebd3c79766123f1b13dcf32a51ef7a818495
ASheddV <ashedd17@gmail.com>
2023-04-21 19:45:47 -0500

    Adding account.py

--- Lib/account.py
+++ Lib/account.py
@@ -1,13 +1,25 @@
 class account:
+"""
+A class cintaining a name and value, representing an account.
+"""
 
 
-    def __init__(self, name):
+    def __init__(self, name: str):
+        """
+        Constructor to create an initial state of a person object.
+        :param name: Person's first name.
+        """
 
         self.__account_name = name
         self.__account_balance = 0
 
 
-        def deposit(self, amount):
+        def deposit(self, amount: float) -> bool:
+            """
+            Function to add money to the account.
+            :param amount: Amount to deposit.
+            :return: Returns True/False. When True is returned, account balance has been updated, with amount added to it.
+            """
             if amount > 0:
                 self.__account_balance = self.__account_balance + amount
                 return True
@@ -16,6 +28,11 @@ class account:
 
 
         def withdraw(self, amount: float) -> bool:
+            """
+            Function to withdraw money from the account.
+            :param amount: Amount to withdraw.
+            :return: Returns True/False. When True is returned, account balance has been updated, with amount removed from it.
+            """
             if amount > 0:
                 if amount <= self.__account_balance:
                     self.__account_balance = self.__account_balance - amount
@@ -27,8 +44,16 @@ class account:
 
 
         def getbalance(self) -> float:
+            """
+            Retrieves Account balance.
+            :return: Returns the account balance.
+            """
             return self.__account_balance
 
 
         def getname(self) -> float:
+            """
+            Retrieves account name.
+            :return: Returns the account name.
+            """
             return self.__account_name
